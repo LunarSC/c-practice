752ffbedd73fe78327991285f352fb0454b17d7b
LunarSC <LunarSC95@gmail.com>
2022-10-10 21:12:45 -0400

    Removing binary files and adding to gitignore. Finished binary tree. Adde to pointer practice script.

--- objects/btree.c
+++ objects/btree.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node
 {
@@ -7,9 +8,46 @@ struct node
     struct node *rightChild;
 };
 
+void print(struct node *node)
+{
+    if (node != NULL)
+    {
+        print(node->leftChild);
+        printf("%i \n", node->data);
+        print(node->rightChild);
+    }
+}
+
+struct node* newNode(int value)
+{
+    struct node *tmp = (struct node *)malloc(sizeof(struct node));
+    tmp->data = value;
+    tmp->leftChild = NULL;
+    tmp->rightChild = NULL;
+    return tmp;
+};
+
+struct node* insertNode(struct node *node, int value)
+{
+    if (node == NULL)
+    {
+        return newNode(value);
+    }
+    if (value < node->data)
+    {
+        node->leftChild = insertNode(node->leftChild, value);
+    }
+    else if (value > node->data)
+    {
+        node->rightChild = insertNode(node->rightChild, value);
+    }
+    return node;
+};
+
 int main()
 {
-    struct node *rootNode = NULL;
-    rootNode->data = 5;
-    printf("%i", rootNode->data);
+    struct node *rootNode = newNode(50);
+    insertNode(rootNode, 25);
+    insertNode(rootNode, 100);
+    print(rootNode);
 }
--- pointers/pointers.c
+++ pointers/pointers.c
@@ -3,10 +3,9 @@
 void printPointers(int arr[], int len)
 {
     int *p = arr;
-    for (int i = 1; i < len + 1; i++)
+    for (int i = 0; i < len; i++)
     {
-        printf("%i ", *p);
-        *(p += i);
+        printf("%i ", *(p++));
     }
 }
 
@@ -26,4 +25,8 @@ int main()
 
     int arr[] = {1, 3, 5, 6};
     printPointers(arr, sizeof(arr) / sizeof(arr[0]));
+
+    int *p = &arr[1];
+    int *q = &arr[2];
+    printf("\np: %p\nq: %p\np - q: %i\n", p, q, *(arr + (q - p)));
 }
