c233d12688eebcf70d722fd33b24429bf78831b5
Tom Pedraza <tpedraza@reingold.com>
2022-10-26 13:23:27 -0400

    Better way of converting string to int

--- primes/primes.c
+++ primes/primes.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <errno.h>
-#include <limits.h>
 #include <stdlib.h>
 
 bool isPrime(int *x)
@@ -28,19 +26,9 @@ int main(int argc, char *argv[])
         printf("No argument provided.");
         return 1;
     }
-    char *param;
-    int x;
 
-    errno = 0;
+    int input = atoi(argv[1]);
 
-    long conv = strtol(argv[1], &param, 10);
-    if (errno != 0 || *param != '\0' || conv > INT_MAX || conv < INT_MIN) {
-        printf("Couldn't convert arg to int!\n");
-        return 1;
-    } else {
-        x = conv;
-    }
-
-    bool prime = isPrime(&x);
-    printf("%i is prime: %s\n", x, prime ? "true" : "false");
+    bool prime = isPrime(&input);
+    printf("%i is prime: %s\n", input, prime ? "true" : "false");
 }
